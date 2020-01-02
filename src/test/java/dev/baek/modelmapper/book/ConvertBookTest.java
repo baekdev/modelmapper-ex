fea174654815d765d5c0b59812521d44b848206f
divetech <diveintotechnology@gmail.com>
2020-01-02 22:14:50 +0900

    TC 추가

--- src/test/java/dev/baek/modelmapper/book/ConvertBookTest.java
+++ src/test/java/dev/baek/modelmapper/book/ConvertBookTest.java
@@ -2,6 +2,7 @@ package dev.baek.modelmapper.book;
 
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -17,30 +18,45 @@ import java.util.Date;
 @RunWith(MockitoJUnitRunner.class)
 public class ConvertBookTest {
 
-	@Test
-	public void test_manual_convert() {
+	private BookDTO bookDTO;
 
-		BookDTO bookDTO = new BookDTO();
+	@Before
+	public void setUp() {
+		bookDTO = new BookDTO();
 		bookDTO.setTitle("How to use Model Mapper?");
+		bookDTO.setSubTitle("Intelligent, Easy, Convention Based, Extensible, Refactoring Safe");
 		bookDTO.setAuthor("BAEKDEV");
 		bookDTO.setPrice(9000L);
 		bookDTO.setPublishedAt(new Date());
 		bookDTO.setCurrencyType(CurrencyType.WON);
-
-		BookVO bookVO = convert(bookDTO);
-		Assertions.assertThat(bookDTO.getAuthor()).isEqualTo(bookVO.getAuthor());
-
 	}
 
-	private BookVO convert(BookDTO bookDTO) {
+	@Test
+	public void test_manual_convert_mismatching_filed() {
+
 		BookVO bookVO = new BookVO();
 		bookVO.setTitle(bookDTO.getTitle());
-		bookVO.setAuthor(bookDTO.getTitle()); // 다른 필드를 set 해줬지만 컴파일 에러가 없기 때문에 인지하기 어렵습니다.
+		bookVO.setAuthor(bookDTO.getTitle()); // 다른 필드를 set 해줬지만 컴파일 에러가 없기 때문에 인지하기 어려움
 		bookVO.setSubTitle(bookDTO.getSubTitle());
 		bookVO.setPrice(bookDTO.getPrice());
 		bookVO.setCurrencyType(bookDTO.getCurrencyType());
 		bookVO.setCreateAt(bookDTO.getPublishedAt());
-		return bookVO;
+
+		Assertions.assertThat(bookDTO.getAuthor()).isNotEqualTo(bookVO.getAuthor());
+	}
+
+	@Test
+	public void test_manual_convert_not_add_mapping_new_filed() {
+
+		BookVO bookVO = new BookVO();
+		bookVO.setTitle(bookDTO.getTitle());
+		bookVO.setAuthor(bookDTO.getAuthor());
+		// bookVO.setSubTitle(bookDTO.getSubTitle()); // subTitle 필드 매핑이 실제로 누락된 것을 주석으로 재연
+		bookVO.setPrice(bookDTO.getPrice());
+		bookVO.setCurrencyType(bookDTO.getCurrencyType());
+		bookVO.setCreateAt(bookDTO.getPublishedAt());
+
+		Assertions.assertThat(bookDTO.getSubTitle()).isNotEqualTo(bookVO.getSubTitle());
 	}
 
 }
